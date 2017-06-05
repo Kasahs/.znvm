e3c31fb2526562822606a7f3073cfbe77d8e0a10
Shasak <shasak.sync@gmail.com>
2017-06-05 12:31:51 +0530

    added main script and install script

---
+++ install.sh
@@ -0,0 +1,31 @@
+#! /bin/zsh
+
+# ensure znvm download location is correct
+local ZNVM_PATH="$HOME/.znvm"
+local ZNVMSH_PATH="$ZNVM_PATH/znvm.sh"
+
+if [ ! -f $ZNVMSH_PATH ]; then
+	echo "ERROR: Could not find $ZNVMSH_PATH. Please ensure $ZNVM_PATH exists."
+	echo "INFO: Follow installation instructions as stated in the README.md file."
+	exit 1
+fi
+
+# ensure zshrc exists at expected location
+local ZSHRC_PATH="$HOME/.zshrc"
+
+if [ ! -f $ZSHRC_PATH ]; then
+	echo "$ZSHRC_PATH does't exist. Aborting"
+	exit 1
+fi
+
+# check if nvm is installed as expected
+local NVM_PATH="$HOME/.nvm/nvm.sh"
+echo $NVM_PATH
+if [ ! -f $NVM_PATH ]; then
+	echo "WARNING: nvm.sh was not found at $NVM_PATH"
+fi
+
+# source znvm.sh inside .zshrc
+echo "source $ZNVMSH_PATH" >> $ZSHRC_PATH
+# # add nvm alias
+# echo "alias nvm='load-nvm'" >> $ZSHRC_PATH 
---
+++ znvm.sh
@@ -0,0 +1,76 @@
+# #############################################################
+# Load nvm only when called explicitly or 
+# when dir contains .nvmrc file.
+# 
+# If .nvmrc is present 
+# automatically [nvm use <node-version specified in .nvmrc>]
+# #############################################################
+znvm_echo() {
+  command printf %s\\n "$*" 2>/dev/null || {
+    znvm_echo() {
+      # shellcheck disable=SC1001
+      \printf %s\\n "$*" # on zsh, `command printf` sometimes fails
+    }
+    znvm_echo "$@"
+  }
+}
+
+znvm_find_up() {
+  local path
+  path="${PWD}"
+  while [ "${path}" != "" ] && [ ! -f "${path}/${1-}" ]; do
+    path=${path%/*}
+  done
+  znvm_echo "${path}"
+}
+
+znvm_find_nvmrc() {
+  local dir
+  dir="$(znvm_find_up '.nvmrc')"
+  if [ -e "${dir}/.nvmrc" ]; then
+    znvm_echo "${dir}/.nvmrc"
+  fi
+}
+
+load-nvm() {
+	unalias nvm
+	echo "loading nvm..."
+	export NVM_DIR="$HOME/.nvm"
+	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+	[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+	# export NVM_LATEST_VERSION="$(ls -1 $NVM_DIR/versions/node | tail -1)"
+	# export PATH=$NVM_DIR/versions/node/$NVM_LATEST_VERSION/bin:$PATH
+	# export MANPATH=$NVM_DIR/versions/node/$NVM_LATEST_VERSION/share/man:$MANPATH
+}
+
+autoload -U add-zsh-hook
+load-nvmrc() {
+	# if current dir has .nvmrc and nvm has not been loaded
+	# load nvm
+	local NVMRC_PATH="$(znvm_find_nvmrc)"
+	if [ -n "$NVMRC_PATH" ] && [ -z "$NVM_DIR" ]; then
+		load-nvm
+	fi
+
+	#ensure nvm has loaded
+	if [ -n "$NVM_DIR" ]; then
+
+		local NODE_VERSION="$(nvm version)"
+
+		if [ -n "$NVMRC_PATH" ]; then
+			local NVMRC_NODE_VERSION=$(nvm version "$(cat "${NVMRC_PATH}")")
+
+			if [ "$NVMRC_NODE_VERSION" = "N/A" ]; then
+				nvm install
+			elif [ "$NVMRC_NODE_VERSION" != "$NODE_VERSION" ]; then
+				nvm use
+			fi
+		elif [ "$NODE_VERSION" != "$(nvm version default)" ]; then
+			echo "Reverting to nvm default version"
+			nvm use default
+		fi
+	fi
+}
+add-zsh-hook chpwd load-nvmrc
+
+alias nvm='load-nvm'
